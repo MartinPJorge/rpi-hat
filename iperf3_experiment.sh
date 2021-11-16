fe6fc25910e73beb90a96676cc90f2d8e086c7fd
lauracaruso2408 <lauracar92@hotmail.it>
2021-11-16 15:02:12 +0000

    Add tcp to iperf3 experiment

--- iperf3_experiment.sh
+++ iperf3_experiment.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-iperf3 -c 10.5.99.101 -u -i 5 -b 10M -t 28800 -J > iperf3.uplink.json
-iperf3 -c 10.5.99.101 -u -i 5 -b 150M -t 28800 -R -J > iperf3.downlink.json
+iperf3 -c 10.5.99.101 -i 5 -t 3600 -R -J > iperf3.downlink.tcp.json
+iperf3 -c 10.5.99.101 -i 5 -t 3600 -J > iperf3.uplink.tcp.json
+iperf3 -c 10.5.99.101 -u -i 5 -b 150M -t 28800 -R -J > iperf3.downlink.udp.json
+iperf3 -c 10.5.99.101 -u -i 5 -b 10M -t 28800 -J > iperf3.uplink.udp.json
+
