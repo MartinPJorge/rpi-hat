37b2f783a27e9a40af8afbf9a60f34b5e0e6c0d9
Jorge Martin Perez <jmartinp@it.uc3m.es>
2021-11-16 17:44:32 +0100

    Wait for <op> select sync

--- connect-5g-sa.sh
+++ connect-5g-sa.sh
@@ -4,6 +4,7 @@
 serial="/dev/ttyUSB3"
 wireless_if="wwan0"
 APN='5gsa02'
+OP_WAIT=60 # time to wait operator selection sync
 
 
 # Ensure raw IP
@@ -15,20 +16,20 @@ sudo ip link set dev wwan0 up
 # issue AT commands 
 echo 'AT+CGDCONT=1,"IP","5gsa02"'
 echo -e 'AT+CGDCONT=1,"IP","5gsa02"\r' > $serial
-sleep 1
+sleep 2
 echo 'AT+CNBP=0x7FFFFFFFFFFFFFFF,0x00000000000000000000000000000000,0x000000000000003F,0x00000000000040000000000000000000,0x00000000000040000000000000000000'
 echo -e 'AT+CNBP=0x7FFFFFFFFFFFFFFF,0x00000000000000000000000000000000,0x000000000000003F,0x00000000000040000000000000000000,0x00000000000040000000000000000000\r' > $serial
-sleep 1
+sleep 2
 echo 'AT+CNMP=71'
 echo -e 'AT+CNMP=71\r' > $serial
-sleep 1
+sleep 2
 echo 'AT+COPS=2'
 echo -e 'AT+COPS=2\r' > $serial
-sleep 1
+sleep 2
 echo 'AT+COPS=1,2,"240680",12'
 echo -e 'AT+COPS=1,2,"240680",12\r' > $serial
-echo "Waiting 10sec for the operator selection..."
-sleep 10
+echo "Waiting $OP_WAIT sec for the operator selection..."
+sleep $OP_WAIT
 
 
 # issue QMI client to open connection
@@ -41,5 +42,5 @@ sudo qmicli -d /dev/cdc-wdm0\
 sudo qmi-network /dev/cdc-wdm0 start
 
 # obtain IP through DHCP
-sudo udhcpc -q -f -n -i $wireless_if
+sudo udhcpc -q -f -i $wireless_if
 
