175c63dcbfdca081b19d649878a231a2e15a3a36
lauracaruso2408 <lauracar92@hotmail.it>
2021-11-16 05:46:49 +0100

    Fix server address

--- iperf3_experiment.sh
+++ iperf3_experiment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-iperf3 -c 10.5.98.2 -u -i 5 -b 10M -t 28800 -J > iperf3.uplink.json
-iperf3 -c 10.5.98.2 -u -i 5 -b 150M -t 28800 -R -J > iperf3.downlink.json
+iperf3 -c 10.5.99.101 -u -i 5 -b 10M -t 28800 -J > iperf3.uplink.json
+iperf3 -c 10.5.99.101 -u -i 5 -b 150M -t 28800 -R -J > iperf3.downlink.json
